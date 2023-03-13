3be73e1ce0621870f14448a236282600897f3e26
Haiexe <tienhaipham06012002@gmail.com>
2023-03-13 21:33:35 +0700

    Khởi tạo giao diện, làm phần header và navber

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// const menuItem = document.querySelectorAll('.menu-item');
+// const list = document.querySelectorAll('.list');
+
+// menuItem.addEventListener('mouseout', () => {
+//   console.log(list.classList)
+// });
